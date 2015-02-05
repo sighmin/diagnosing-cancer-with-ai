d65033656ca343c157906fb2a6e53b631c370421
Simon van Dyk <simon.vandyk@gmail.com>
2015-02-05 23:08:20 +0200

    test

--- lib/intelligence.rb
+++ lib/intelligence.rb
@@ -100,7 +100,7 @@ module Intelligence
     end
 
     def train_for_cancer
-      simulator = Intelligence::Sims::NnSimulator.new do
+      simulator = Sims::NnSimulator.new do
         simulation type: :simulation, runs: 1, resolution: 1
         algorithm  type: :pso, iterations: 10, population: 35, dimensions: 225, domain: (-0.5..0.5), vdomain: (-0.7..0.7)
         problem    type: :nn_training_problem,
